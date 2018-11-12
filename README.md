613e75fdfd42a8906a4b32ec6827e9b7a666b40d
Yi He <yi_he@apple.com>
2018-11-11 23:42:38 -0800

    added info for rouge and start_job on aws

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # yi_can_summarize
 CS-230 Project Yi He, Can He
+
--- start_job.sh
+++ start_job.sh
@@ -2,7 +2,7 @@
 
 mkdir data
 
-#Install Bazel
+#Install Bazel, need for building textsum
 sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python
 wget "https://github.com/bazelbuild/bazel/releases/download/0.18.1/bazel-0.18.1-installer-linux-x86_64.sh"
 chmod +x bazel-0.18.1-installer-linux-x86_64.sh
@@ -10,12 +10,15 @@ chmod +x bazel-0.18.1-installer-linux-x86_64.sh
 
 bazel build -c opt textsum/...
 
+#create virtual environment
 sudo apt install virtualenv
 cd ..
 virtualenv venv
 source venv/bin/activiate
 pip install -r yi_can_summarize/requirements.txt
+pip install git+https://github.com/tagucci/pythonrouge.git #install rouge-calculation package
 
+#download cnn new articles from S3 account
 cd yi_can_summarize
 mkdir data
 cd data
@@ -27,21 +30,37 @@ cd ..
 python textsum_data_convert.py \
   --command text_to_vocabulary \
   --in_directories data/cnn/stories \
-  --out_files data/cnn-vocab.bin
+  --out_files data/vocab
 
 python textsum_data_convert.py \
   --command text_to_binary \
   --in_directories data/cnn/stories \
-  --out_files data/cnn-train.bin,data/cnn-validation.bin,data/cnn-test.bin \
+  --out_files data/train.bin, data/valid.bin,data/test.bin \
   --split 0.8,0.15,0.05
 
 bazel-bin/textsum/seq2seq_attention \
   --mode=train \
   --article_key=article \
   --abstract_key=abstract \
-  --data_path=data/cnn-train.bin \
-  --vocab_path=data/cnn-vocab.bin \
-  --log_root=log_root \
-  --train_dir=log_root/train \
+  --data_path=data/train.bin \
+  --vocab_path=data/vocab \
+  --log_root=log \
+  --train_dir=log/train \
   --truncate_input=True
 
+#test
+bazel-bin/textsum/seq2seq_attention \
+  --mode=decode \
+  --article_key=article \
+  --abstract_key=abstract \
+  --data_path=data/test.bin \
+  --vocab_path=data/vocab.bin \
+  --log_root=log \
+  --decode_dir=log/decode \
+  --beam_size=8 \
+  --truncate_input=True
+
+#check the rouge score
+python rouge_calculation.py \
+ --reference /log/decode/ref \
+ --summary /log/decode/summary
